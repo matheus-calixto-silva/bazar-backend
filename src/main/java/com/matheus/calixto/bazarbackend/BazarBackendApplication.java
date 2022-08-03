4b1b25b484a773b0db7decbafad9a720d50ad8f9
Matheus Calixto <matheuscalixto8@gmail.com>
2022-08-03 17:16:50 -0300

    commit inicial

---
+++ src/main/java/com/matheus/calixto/bazarbackend/BazarBackendApplication.java
@@ -0,0 +1,13 @@
+package com.matheus.calixto.bazarbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BazarBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BazarBackendApplication.class, args);
+	}
+
+}
