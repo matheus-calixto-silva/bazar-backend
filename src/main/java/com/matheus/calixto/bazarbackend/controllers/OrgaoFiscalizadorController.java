900f018c2b0591036cb7b7aa262d801dde26e75a
Matheus Calixto <matheuscalixto8@gmail.com>
2022-08-06 13:05:05 -0300

    refactor: atualizando url do orgao fiscalizador

--- src/main/java/com/matheus/calixto/bazarbackend/controllers/OrgaoFiscalizadorController.java
+++ src/main/java/com/matheus/calixto/bazarbackend/controllers/OrgaoFiscalizadorController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.NoSuchElementException;
 
 @RestController
-@RequestMapping("/api/orgaos")
+@RequestMapping("/api/orgaos/fiscalizador")
 public class OrgaoFiscalizadorController {
     @Autowired
     OrgaoFiscalizadorService orgaoFiscalizadorService;
